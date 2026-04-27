f424ae6018c3ed5d15a35d32cdfab4b27639c942
sasiyada03 <rac00n.mellow@gmail.com>
2026-04-27 18:35:02 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+a = float(input("Give me the first number: ").strip())
+b = float(input("Give me the second number: ").strip())
+print("Thank you!")
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} * {b} = {a * b}")
---
+++ cell04/ex03/float.py
@@ -0,0 +1,5 @@
+num = float(input("Give me a number: ").strip())
+if num.is_integer():
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
